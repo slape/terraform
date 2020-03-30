3459ee0f3fc85ec6a1a39053fc9335243bd4ee32
Isaac Slape <sslape@2u.com>
2020-03-30 10:50:01 -0400

    Definied initial web security group

--- prod.tf
+++ prod.tf
@@ -3,11 +3,39 @@ provider "aws" {
 	region	= "us-west-1"
 }
 
-resource "aws_s3_bucket" "prod_tf_course" {
-	bucket = "tf-course-bable"
+resource "aws_s3_bucket" "tf_babble" {
+	bucket = "tf-course-babble"
 	acl = "private"
 }
 
 resource "aws_default_vpc" "default" {
 
+}
+
+resource "aws_security_group" "home-web" {
+	name 				= "home-web"
+	description = "Allow standard http(s) ports inbound."
+	
+	ingress {
+		from_port   = 80
+		to_port     = 80
+		protocol    = "tcp"
+		cidr_blocks = ["24.98.49.20/32"]
+	}
+	ingress {
+		from_port   = 443
+		to_port     = 443
+		protocol    = "tcp"
+		cidr_blocks = ["24.98.49.20/32"]
+	}
+	egress {
+		from_port   = 0
+		to_port     = 0
+		protocol    = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	tags = {
+		"Terraform" : "true"
+	}
 }
